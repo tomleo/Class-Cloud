7cb2b8a235d0f24475f0fdc282e8ccf6b447b6cb
Tom <tomjleo@gmail.com>
2012-06-11 19:01:33 -0400

    Added basic assignment view

--- class_cloud/assignments/admin.py
+++ class_cloud/assignments/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from assignments.models import Teacher, Student, Assignment, Course, Grade
+#from assignments.models import Teacher, Student, Assignment, Course, Grade
+from assignments.models import Assignment
 
-admin.site.register(Teacher)
-admin.site.register(Student)
+#admin.site.register(Teacher)
+#admin.site.register(Student)
 admin.site.register(Assignment)
-admin.site.register(Course)
-admin.site.register(Grade)
+#admin.site.register(Course)
+#admin.site.register(Grade)
--- class_cloud/assignments/views.py
+++ class_cloud/assignments/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from models import Assignment
+
+def assignments(request):
+    list_of_assignments = Assignment.objects.all()
+    return render_to_response('assignments.html', {
+        'list_assignments': list_of_assignments,
+    })
