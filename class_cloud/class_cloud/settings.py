f77e2da05d3797d517a03d3c3f0692c5d5577ca7
Tom <tomjleo@gmail.com>
2012-07-18 03:16:40 -0400

    Made meda root relative

--- class_cloud/class_cloud/settings.py
+++ class_cloud/class_cloud/settings.py
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/shresthad/abc/Class-Cloud/class_cloud/media/'
+MEDIA_ROOT = rel_path('../media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
