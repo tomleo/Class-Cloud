7e19f7417e4ac408188b25d4f778a09e2559361e
Tom <tomjleo@gmail.com>
2012-07-20 11:11:47 -0400

    Resolving merge conflict

--- class_cloud/course/views.py
+++ class_cloud/course/views.py
@@ -2,7 +2,7 @@
 from django.views.generic import list_detail, date_based, TemplateView, RedirectView, DetailView
 from django.views.generic.edit import FormView
 
-from course.models import Course, Assignment, Grade, StudentGrade
+from course.models import Course, Assignment, Grade, StudentGrade, SubmittedAssignment
 from django.contrib.auth.models import User
 
 from django.shortcuts import render_to_response, RequestContext
@@ -15,18 +15,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
 
+
 def calendar(request):
     return render_to_response('calendar.html',
         context_instance=RequestContext(request))
 
+
 def passign(request):
     return render_to_response('passign.html',
         context_instance=RequestContext(request))
-        
+
+
 def course_grades(request):
     return render_to_response('course_grades.html',
         context_instance=RequestContext(request))        
-        
 
 
 @login_required
@@ -50,26 +52,90 @@ def courses(request, slug):
     return render_to_response('courses.html',
         {'courses': courses},
         context_instance=RequestContext(request))
-                                    
-                                    
+
+def course_test(request, course_slug):
+    
+    selected_course = Course.objects.get(slug=course_slug)
+    course_assignments = Assignment.objects.filter(course=selected_course)
+    
+    import pdb; pdb.set_trace()
+    
+    return HttpResponse(course_assignments)
+    
+
 @login_required
 @user_passes_test(lambda u: u.has_perm('course.student_view'))
 def course(request, slug):
 
+    # Assignments Not Submitted
+    # Assignments Submitted
+    # Assignments Submitted and Graded
+
     selected_course = Course.objects.get(slug=slug)
+
+    # Need to further filter this by due dates and wheather the assignment is active
     course_assignments = Assignment.objects.filter(course=selected_course)
-    #grades = Grade.objects.filter(course=selected_course)
     
-    class_grades = []
-    grades = StudentGrade.objects.filter(student__username=request.user.username,
+    # Need to filter this by course
+    submitted = SubmittedAssignment.objects.filter(student__username=request.user.username)
+    
+    # all grades associated with given course and user
+    student_grades = StudentGrade.objects.filter(student__username=request.user.username,
                                           assignment__course=selected_course)
-    class_grades = grades
+
+    submitted_and_graded = []
+    for assignment in submitted:
+        if assignment in [a.assignment for a in student_grades]:
+            submitted_and_graded.append(assignment)
+    
+    #def isInAssignmentList(submitted, G):
+    #    for (index, a) in enumerate(submitted):
+            #if G.assignment in [a.assignment for a in L[index]]:
+    #        if G.assignment in submitted[index]:
+    #            return (G, submitted[index])
+   
+    a_graded = []
+    a_submitted = []
+    a_uncomplete = []
+    
+    for grade in student_grades:
+        #for assignment in enumerate(index, submitted_and_graded):
+        #    if grade.assignment in submitted_and_graded[index]:
+        #        a_graded.append((grade,submitted_and_graded[index]))
+        
+        # Check if any submitted assignments have grades
+        
+        #assignment_tuple = isInAssignmentList(submitted, grade)
+        #if assignment_tuple:
+        #    a_graded.append(assignment_tuple)
+
+        for assignment in submitted:
+            for student_grade in student_grades:
+                if assignment == student_grade.assignment:
+                    a_graded.append((student_grade.grade, assignment))
+        
+    # Find submitted assignments that are not graded
+    for sa in submitted:
+        if sa not in [a[1] for a in a_graded]:
+            a_submitted.append(sa)
+    
+    # Find assignments that have not been submitted
+    for a in course_assignments:
+        if a not in [i[0] for i in a_graded]:
+            if a not in a_submitted:
+                a_uncomplete.append(a)
+    
+    
+
     
     template_name = 'course.html'
     return render_to_response(template_name, 
         {'course':selected_course,
-         'assignments':course_assignments,
-          'grades':class_grades},
+         'assignments_graded':a_graded,
+         'assignments_submitted':a_submitted,
+         'assignments_inbox':a_uncomplete,
+         #'assignments':course_assignments,
+         'grades':student_grades},
         context_instance=RequestContext(request))
     
 
